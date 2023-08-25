b0419a3ac6b901f9fc64a50640412b3fd56f68dc
Phu <vanphu040520022gmail.com>
2023-08-25 16:54:10 +0700

    Change mongoDB network

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1/live_code');
+        await mongoose.connect('mongodb+srv://phunguyen:Phu123123@cluster0.tgh56po.mongodb.net/');
         console.log("Connect successfully!");
     } catch (error) {
         console.log("Connect failure!");
